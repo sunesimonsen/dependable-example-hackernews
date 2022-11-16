2f20dfda049a47541f999dea15cd84d83fb83f3c
Sune Simonsen <sune@we-knowhow.dk>
2022-11-16 01:17:38 +0100

    Fix casing typo

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ import { html, render } from "@dependable/view";
 import { RootView } from "./components/RootView.js";
 import { Router, Routing } from "@dependable/nano-router";
 import { createBrowserHistory } from "@nano-router/history";
-import { Api } from "./Api.js";
+import { Api } from "./api.js";
 import { routes } from "./routes.js";
 
 const history = createBrowserHistory();
