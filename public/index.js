3ed7ae6b42efe89f492bce48c1aa1631621b728b
Sune Simonsen <sune@we-knowhow.dk>
2023-12-21 12:42:46 +0100

    Switch to Vite as the devserver

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+import "@dependable/vite";
 import { html, render } from "@dependable/view";
 import { RootView } from "./components/RootView.js";
 import { Router, Routing } from "@dependable/nano-router";
